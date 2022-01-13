de6dece96ca1acbaa505287616c4d672add416b3
Julia Pineda <Julia.Pineda@analog.com>
2022-01-13 15:17:12 +0800

    LinuxStage: Add missing drivers in description
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- src/sdg/Gauntlet.groovy
+++ src/sdg/Gauntlet.groovy
@@ -378,12 +378,10 @@ def stage_library(String stage_name) {
                             try {
                                 carrier = nebula('update-config board-config carrier --board-name='+board )
                                 daughter = nebula('update-config board-config daughter --board-name='+board )
-                                description += "Missing drivers: " + missing_devs.size().toString() + " (" + missing_devs.join(",") + ")\n"
-                                // description += "\n"+failed_test
+                                description += "Missing drivers: " + missing_devs.size().toString() + " (" + missing_devs.join(", ") + ")\n"
                                 dmesg_err_count = sh(returnStdout: true, script: 'cat dmesg_err_filtered.log | wc -l').trim()
-                                // dmesg_errs = readFile 'dmesg_err_filtered.log'
-                                dmesg_errs = new File("dmesg_err_filtered.log").text.readLines()
-                                println(dmesg_errs.size())
+                                dmesg_errs = readFile("dmesg_err_filtered.log").readLines()
+                                println(dmesg_errs.size());
                                 description += "dmesg errors: ${dmesg_err_count}\n" + dmesg_errs
                                 description = "\n{color:#de350b}*"+get_gitsha(board).toMapString()+"*{color}\n".concat(description)
                             } catch(Exception desc){
